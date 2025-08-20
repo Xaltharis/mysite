648bea8df75d01333d64bfd5ce03367cd1ad7815
Xaltharis <justwinte@gmail.com>
2025-08-20 18:43:59 +0300

    Initial commit: Set up mysite project structure

---
+++ movies/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='home' ),
+    path('about/', about, name='about'),
+    path('addmovie/', add_movie, name='add_movie'),
+    path('post/<int:post_id>/', show_movie, name='post'),
+    path('director/<int:director_id>/', show_director, name='director'),
+]
---
+++ movies/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+
+from .models import Movie
+
+menu = [
+    {'title': 'Главная', 'url_name': 'home'},
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить фильм', 'url_name': 'add_movie'},
+]
+
+def index(request):
+    movies = Movie.objects.all()
+    param = {
+        'menu': menu,
+        'movies': movies,
+        'title': 'Главная страница'
+    }
+    return render(request, 'movies/index.html', context=param)
+
+def about(request):
+    return render(request, 'movies/about.html', {'menu': menu, 'title': 'О сайте'})
+
+def add_movie(request):
+    return HttpResponse('Добавление фильма')
+
+def show_movie(request, post_id):
+    return HttpResponse(f'Страница фильма с ID = {post_id}')
+
+def show_director(request, director_id):
+    return HttpResponse(f'Страница режиссёра с ID = {director_id}')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('Страница не найдена')
